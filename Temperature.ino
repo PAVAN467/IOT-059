e7621e37c2c52ecbf699b6f1d65928e879d13c32
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2019-09-04 19:34:31 +0530

    Update Temperature.ino

--- Temperature.ino
+++ Temperature.ino
@@ -1,11 +1,11 @@
 int analogoutput=A0;
 void setup() {
-  // put your setup code here, to run once:
+
     Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
     int sensevalue=analogRead(analogoutput);
     float milivolts=(sensevalue/1024.0)*500;
     float celsius=milivolts/10;
